0cfcc82d271c568e74c6ca9f3be6b1bd023af055
Bullrio <bullriopfp@gmail.com>
2025-06-11 12:05:11 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>Welcome to TrackFrame</h1>
-      <p>Paste a YouTube or Spotify link to generate a Frame.</p>
-    </main>
-  );
-}
